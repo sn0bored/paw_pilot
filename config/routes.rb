52aa79c6bf1182b3e4640b4b1f0bdc26c7503823
sn0bored <dmkwillems@gmail.com>
2024-09-28 11:26:21 -0400

    adds basic sidebar and nav

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,15 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  authenticated :user do
-    root to: 'dashboard#index', as: :authenticated_root
+  devise_scope :user do
+    authenticated :user do
+      root to: 'dashboard#index', as: :authenticated_root
+    end
+    
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
-  root to: 'devise/sessions#new'
+
+  resources :vans
 end
