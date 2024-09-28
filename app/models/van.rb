4376dfab556c435bc3856762c3f02e236c377fee
sn0bored <dmkwillems@gmail.com>
2024-09-28 10:30:46 -0400

    sets default capacity on vans.  leaves room for larger or smaller in the future

--- app/models/van.rb
+++ app/models/van.rb
@@ -2,4 +2,14 @@ class Van < ApplicationRecord
   has_many :assignments
   has_many :dog_walkers, through: :assignments, source: :user
   has_many :dogs, through: :assignments
+
+  before_save :set_default_capacity
+
+  DEFAULT_CAPACITY = 12
+
+  private
+
+  def set_default_capacity
+    self.capacity = DEFAULT_CAPACITY if capacity.blank?
+  end
 end
